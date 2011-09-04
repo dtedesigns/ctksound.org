a51253c501588ecdfa3f3d15fb30d9d89ac205ba
Kevin Gustavson <kdgustavson@gmail.com>
2011-09-03 23:19:06 -0500

    Sermon and Aces are merged and are sorted.

--- application.rb
+++ application.rb
@@ -6,20 +6,19 @@ require 'sinatra/sequel'
 require 'logger'
 require 'less'      # LESS CSS templates
 require 'erb'
-require 'Sermon'
+#require 'Sermon'
+require 'yaml'
 
 #require 'haml'
 #require 'rdiscount' # for markdown
 #require 'redcloth'  # for textile
 
-#require 'fileutils'
-#require 'ftools'
-
 #require 'partial_helper'
 #set :textile, :layout_engine => :erb
 #set :markdown, :layout_engine => :erb
 
-set :database, 'mysql://kgustavson:kgustavson11@localhost/ctksound'
+#set :database, 'mysql://kgustavson:kgustavson11@localhost/ctksound'
+set :database, 'mysql://sermons:sermons@gandalf/sermons'
 
 class Sermons < Sequel::Model
     def validate
@@ -27,7 +26,6 @@ class Sermons < Sequel::Model
         errors.add(:title, 'cannot be empty') if !title || title.empty?
     end
 end
-#Sermons.row_proc = proc{|row| erb :sermon_record, :locals => { :data => row }, :layout => false }
 
 # use Rack::Auth::Basic "Restricted Area" do |username, password|
 #     [username, password] == ['admin','admin']
@@ -44,129 +42,101 @@ end
         #throw(:halt, [401, "Not authorized\n"])
     #end
 #end
-#DB = Sequel.connect('mysql://kgustavson:kgustavson11@localhost/ctksound', :logger => Logger.new('log/ctksound_db.log'))
-
-def getData
-    rows = []
-
-    sermons = DB['select * from sermons'].limit(10).order(:date.desc)
-    sermons.each() do |sermon|
-        rows.push(erb :sermon_record, :locals => { :data => sermon }, :layout => false)
-    end
-
-    #portraits = DB['select * from portraits'].limit(10)
-    #aces = DB['select * from aces'].limit(10)
-    #dedications = DB['select * from dedications'].limit(10)
-
-    rows
-end
 
 
 get '/' do
-    sermons = database[:sermons].filter('date > 2011-01-01').order(:date.desc)
-    erb :"wireframe", :locals => { :data => sermons }
+    dates = Hash.new #{ |l, k| l[k] = Hash.new(&l.default_proc) }
+    aces = database[:aces].filter().order(:date)
+    portraits = database[:portraits].filter().order(:date)
+    sermons = database[:sermons].filter(:type => 'Sermons').order(:date.desc)
+
+    sermons.each do |sermon|
+        date = sermon[:date].to_s+'_0_sermon' #.strftime('%s_0_sermon')
+        dates[date] = liquid(:sermon_record, :layout => false, :locals => sermon)
+    end
+
+    aces.each do |ace|
+        date = ace[:date].to_s+'_1_ace' #.strftime('%s_0_ace')
+        dates[date] = liquid(:ace_record, :layout => false, :locals => ace)
+    end
+
+    #erb :"wireframe", :locals => { :data => sermons }
+    #items = {}
+    #dates.keys.sort.each do |key|
+        #items[key] = dates[key]
+    #end
+    liquid :wireframe, :layout_engine => :erb, :locals => {
+        :filters => erb(:filters, :layout => false),
+        :items => dates,
+        :keys => dates.keys.sort,
+    }
 end
 
-# Possible stylesheet route
+# stylesheet route
 get '/style/:name' do |n|
     less :"style/#{n}"
 end
 
-get '/default.css' do
-    #"put template here"
-    less :"style/default"
-end
-
-get '/sequel' do
-    rows = []
-
-    DB = Sequel.connect('mysql://kgustavson:kgustavson11@localhost/ctksound', :logger => Logger.new('log/ctksound_db.log'))
-
-    portraits = DB['select date,speaker,comment,created_at,updated_at from portraits']
-    portraits = portraits.filter("comment like '%test%'")
-    puts "filtered: #{portraits.count}"
-    portraits.order(:date.desc).each do |row|
-        rows += [row.to_s]
-    end
-
-    rows.join('<br>')
-
-    #sermons = DB[:sermons]
-    #p sermons.count
-    #p sermons.first
-
-    #DB['desc portraits'].each do |col|
-        #p col
-    #end
-end
-
-get '/test' do
-    #protected!
-    str = ''
-    Dir.foreach('views/event') { |f|
-        next if f.index('.') == 0
-        str << "#{f}<br>"
-    }
-    str
-end
-
-#post '/upload/:name/:filename' do
-    #userdir = File.join("files" , params[:name])
-    #FileUtils.mkdir_p(userdir)
-    #filename = File.join(userdir, params[:filename])
-    #datafile = params[:data]
-    #"#{datafile[:tempfile].inspect}\n"
-#
-    #File.copy(datafile[:tempfile], filename)
-    #File.open(filename, 'wb') do |file|
-    # file.write(datafile[:tempfile].read)
-    #end
-    #"wrote to #{filename}\n"
-#end
 
 # upload with:
 # curl -v -F "data=@/path/to/filename" http://localhost:4567/user/filename
-
-post '/something' do 
-    #if ( ['recordings','labels','Originals'].include? params[:type] )
-        datafile = params[:data]
-        userdir = File.join('/var/www/sound/webroot', 'recordings')
-        filename = File.join(userdir, '2011-07-31')
-        FileUtils.mkdir_p(userdir)
-        File.open(filename, 'wb') do |file|
-            file.write(datafile[:tempfile].read)
-        end
-        mimetype = `file -Ib #{filename}`.gsub(/\n/,"")
-        "wrote file of #{mimetype} to #{filename}\n"
-    #else
-        #"this is an invalid file type (#{mimetype}\n"
-    #end
+post '/:user/:file' do 
+    data = params[:data]
+    userdir = File.join('/var/www/sound/webroot', 'recordings')
+    filename = File.join(userdir, data[:file])
+    FileUtils.mkdir_p(userdir)
+    File.open(filename, 'wb') do |file|
+        file.write(data[:tempfile].read)
+    end
+    mimetype = `file -Ib #{filename}`.gsub(/\n/,"")
+    "wrote file of #{mimetype} to #{filename}\n"
+    #"this is an invalid file type (#{mimetype}\n"
 end
 
-get '/dndupload' do
-    erb :index
-end
+post '/upload' do
+    # Destination
+    upload_path = 'files/'
+    upload = params["'upload'"]
 
-put '/something' do
-    datafile = params[:data]
-    #userdir = File.join('/var/www/sound/webroot', 'recordings')
-    #FileUtils.mkdir_p(userdir)
-    filename = "/var/www/sound/webroot/recordings/2011-07-31"
-    #FileUtils.cp(datafile[:tempfile], filename)
-    #File.open(filename, 'wb') do |file|
-        #file.write(datafile[:tempfile].read)
-    #end
-    #mimetype = `file -Ib #{filename}`.gsub(/\n/,"")
-    #"wrote file of #{mimetype} to #{filename}\n"
-    #"uploaded #{env['HTTP_X_FILENAME']} - #{request.body.read.size} bytes"
-end
+    File.open(upload_path+upload[:filename], 'wb') do |file|
+        file.write(upload[:tempfile].read)
+    end
+                    #if(count($_FILES)>0) do
+                        #$_FILES.each() do |file|
+                            #data = params[:data]
+                            #File.mv(upload_path+data['upload']['name'], data['upload']['tmp_name'])
+                        #end
+                    #else (isset($_GET['up']))
+                    #// If the browser does not support sendAsBinary ()
+                    #if(isset($_GET['base64'])) {
+                        #$content = base64_decode(file_get_contents('php://input'));
+                    #} else {
+                        #$content = file_get_contents('php://input');
+                    #}
 
+                    #$headers = getallheaders();
+                    #$headers = array_change_key_case($headers, CASE_UPPER);
+
+                    #//if(file_put_contents($upload_folder.'/'.$headers['UP-FILENAME'], $content)) {
+                    #//  echo 'done';
+                    #//}
+                    #exit();
+                #}
+end
 
 put '/' do
-  tempfile = params['file'][:tempfile]
-  filename = params['file'][:filename]
-  File.mv(tempfile.path,File.join(File.expand_path(File.dirname(File.dirname(__FILE__))),"public","#{filename}"))
-  #redirect '/'
+    file = File.open('files/'+request.env['HTTP_X_FILENAME'], 'w+')
+    request.body.read do |io|
+        pos = io.tell
+        while buffer = io.read(2**8)
+            file.write(pos, buffer)
+            pos = io.tell
+        end
+        file.close
+    end
+
+    media_type = request.media_type
+    #YAML::dump(request.body.read)
 end
 
 #curl -v --location --upload-file file.txt http://localhost:4567/upload/
--- config.ru
+++ config.ru
@@ -1,14 +1,9 @@
-#require 'rubygems'
-#require 'sinatra'
+require 'rubygems'
+require 'sinatra'
 require 'application'
 
 set :run, false
 set :environment, :development
-
-#Sinatra::Application.default_options.merge!(
-#  :run => false,
-#  :env => ENV['RACK_ENV']
-#)
 
 FileUtils.mkdir_p 'log' unless File.exists?('log')
 log = File.new("log/sinatra.log", "a")
@@ -16,3 +11,8 @@ $stdout.reopen(log)
 $stderr.reopen(log)
 
 run Sinatra::Application
+
+#require 'rubygems'
+#require 'modular_application'
+#
+#run Postie::Application
