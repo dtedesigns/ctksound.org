110996bb64b724007c5e139722b494fc3ab2bff3
kevin <kevin@0b620e26-4ce5-40c4-bee8-f927bbdcbfe5>
2013-02-01 02:46:11 +0000

    Updating tons of changes.
    git-svn-id: svn+ssh://gandalf/var/REPO/ctksound/trunk@33 0b620e26-4ce5-40c4-bee8-f927bbdcbfe5

---
+++ application/views/user/login.php
@@ -0,0 +1,18 @@
+<h1>Welcome! Please log in.</h1>
+
+<?php if (isset($message)): ?>
+	<div class="error" style="color:red;">
+		<?= $message ?>
+	</div>
+<?php endif ?>
+
+<?php echo form::open('/user/login');
+	echo form::label('username','Username:');
+	echo form::input('username', $username); ?>
+	<br/>
+	<?php echo form::label('password','Password:'); ?>
+	<?php echo form::password('password'); ?>
+	<br/>
+	<?php echo form::submit('submit','Login'); ?>
+
+<?php echo form::close(); ?>
