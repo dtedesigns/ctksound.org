8b7040dd19701bf56e4cf675baf65e73550235d8
kevin <kevin@0b620e26-4ce5-40c4-bee8-f927bbdcbfe5>
2013-02-01 02:48:19 +0000

    Updating tons of changes.
    git-svn-id: svn+ssh://gandalf/var/REPO/ctksound/trunk@34 0b620e26-4ce5-40c4-bee8-f927bbdcbfe5

---
+++ application/views/documents.php
@@ -0,0 +1,112 @@
+<?php
+// vim: set et fenc= ff=unix sts=2 sw=2 ts=2
+?>
+
+  <div id="documents">
+    <p>
+      <a href="/documents/alesis_masterlink_productoverview.pdf">MasterLink Product Overview</a>
+    </p>
+    <p>
+      <a href="/documents/masterlink_manual.pdf">MasterLink Manual</a>
+    </p>
+    <p>
+      <a href="/documents/masterlink_manual_v2.pdf">MasterLink Addendum for Version 2.0</a>
+    </p>
+
+    <div class="subdocument_action" onclick="javascript:$('#contacts').slideToggle('slow');">
+      <div style="float:right; color:#eee;">Click to expand&nbsp;</div>
+      Sound Team Volunteer Contacts
+    </div>
+    <div style="display:none;" id="contacts" class="subdocument">
+      <p style="margin-top: 0;">
+        Kevin Gustavson<br/>
+        471 Kickapoo Ave.<br/>
+        Batavia, IL 60510<br/>
+        Home: (630) 406-1178<br/>
+        Mobile: (630) 913-0016<br/>
+        Email: kgust@pobox.com<br/>
+        <em>Prefered method of contact: email or phone</em>
+      </p>
+      <p>&nbsp;</p>
+    </div>
+
+    <p> </p>
+
+    <div class="subdocument_action" onclick="javascript:$('#responsibilities').slideToggle('slow');">
+      <div style="float:right; color:#eee;">Click to expand&nbsp;</div>
+      Sound Team Volunteer Responsibilities
+    </div>
+    <div style="display:none;" id="responsibilities" class="subdocument">
+    <h1>
+      <font size=5>Sound Team Volunteer - Job Description</font>
+    </h1>
+    <h2>On your given week you are responsible for:</h2>
+      <h3>Setup and Practice</h3>
+      <ul>
+        <li>Getting sound equipment set up</li>
+        <ul>
+          <li>Music Team would like equipment ready by 8:15 am</li>
+        </ul>
+        <li>Adjusting the mixer board and supporting the music team during practice</li>
+        <li>Preparing ML-9600 playlists for recording</li>
+      </ul>
+      <h3>Adult Christian Education (ACE)</h3>
+      <ul>
+        <li>Recording teaching time (except during videos)</li>
+        <li>Burning a CD</li>
+      </ul>
+      <h3>Service</h3>
+      <ul>
+        <li>Making sure preacher has a wireless microphone and the microphone has a good battery</li>
+        <li>Managing mixer during service</li>
+        <li>Recording special events</li>
+        <ul>
+          <li>Portrait of Grace</li>
+          <li>Dedications and Infant Baptisms</li>
+          <li>Etc.</li>
+        </ul>
+        <li>Recording the Scripture Reading, Sermon and Benediction</li>
+        <li>Burning a sermon CD</li>
+      </ul>
+      <h3>Tear-down</h3>
+      <ul>
+        <li>Detach snake, shutdown and secure mixer board</li>
+        <li>Wrap up snake</li>
+        <li>Help others to tear-down sound equipment</li>
+        <li>Deliver both CDs to post-processor (usually Kevin)</li>
+      </ul>
+    </ul>
+    <h2>Keep in mind:</h2>
+    <ul>
+      <li>You are free to switch weeks find substitutes, etc. Please inform the leader of all schedule changes.</li>
+      <li>If you find you are unable to serve on your scheduled day due to illness or other causes, please call and speak to at least one person so that proper action can be taken. The list would include:</li>
+      <ul>
+          <li>The Sound Team Leader (if unavailable, please leave a message)</li>
+          <li>Another Sound Team member</li>
+          <li>Bill or Megan Gabriel</li>
+          <li>The deacon in charge of setup</li>
+        </ul>
+        <li>Inform Leader of any equipment problems or potential problems.</li>
+        <li>Your leader is there to help you serve, make use of him.</li>
+        <li>There are equipment manuals available online, spend the time to learn the equipment.</li>
+      </ul>
+      <h3>Thanks for serving!!! &nbsp;Christ the King appreciates you.</h3>
+      <p>&nbsp;</p>
+    </div>
+
+    <p> </p>
+
+    <div class="subdocument_action" onclick="javascript:$('#commands').slideToggle('slow');">
+      <div style="float:right; color:#eee;">Click to expand&nbsp;</div>
+      Useful commands
+    </div>
+    <div style="display:none;" id="commands" class="subdocument">
+      <h1>Linux commandline tools</h1>
+      <ul>
+        <li><code>sudo wodim dev=/dev/cdrw -v speed=16 -dao -text cuefile=cti_wave.cue</code></li>
+        <li><code>icedax -D /dev/cdrom -J</code></li>
+      </ul>
+      <p>&nbsp;</p>
+    </div>
+
+  </div>
