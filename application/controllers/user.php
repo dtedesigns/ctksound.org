9c97000588c45552d7e23d2aa6f95be8ec5f1cc2
Kevin Gustavson <kdgustavson@gmail.com>
2013-09-01 14:06:58 -0500

    Created a auth.php.dist config file.

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -4,69 +4,61 @@
  */
 class User_Controller extends Template_Controller {
 
-	// Set the name of the template to use
-	public $template = 'welcome_content';
+    // Set the name of the template to use
+    public $template = 'welcome_content';
 
-	public function __construct()
-	{
-		parent::__construct();
-		//var_dump($_REQUEST);
-		//var_dump($_SESSION);
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        //var_dump($_REQUEST);
+        //var_dump($_SESSION);
+    }
 
-	public function index($role = "")
-	{
-		//$this->force_login('admin');
-		$this->login($role);
-	}
+    public function index($role = "")
+    {
+        //$this->force_login('admin');
+        $this->login($role);
+    }
 
-	public function login($role = "")
-	{
-		$form = $_POST;
+    public function login($role = "")
+    {
+        $form = $_POST;
 
-		if ($_POST)
-		{
-			// Load the user
-			//Auth::instance()->login($form['username'], $form['password'],true);
-			$users = Kohana::config('auth.users');
-			if($users[$form['username']] == $form['password']) {
-				Auth::instance()->force_login($form['username']);
-			}
+        if ($_POST)
+        {
+            // Load the user
+            $users = Kohana::config('auth.users');
+            Auth::instance()->login($form['username'], $form['password'],true);
+            //Auth::instance()->force_login($form['username']);
 
-			var_dump(
-				$form['username'],
-				$form['password'],
-				Auth::instance()->hash_password($password,false),
-				Auth::instance()->find_salt(Kohana::config('auth.users.kevin')),
-				Auth::instance()->hash_password($password, Auth::instance()->find_salt(Kohana::config('auth.users.kevin')))
-			); //die();
-		}
+            //$this->templates->message('Invalid Credentials.');
+        }
 
-		if (Auth::instance()->logged_in($role))
-		{
-			// return to page where login was called
-			//if ($this->session->get('requested_url')) {
-			//	url::redirect($this->session->get('requested_url'));
-			//} else {
-				url::redirect('/dash/');
-			//}
-		}
-		else
-		{
-			if (Auth::instance()->logged_in()) {
-				$this->template->title = "No Access";
-				$this->template->content = new View('/user/noaccess');
-			} else {
-				$this->template->title = "Please Login";
-				$this->template->content = new View('/user/login');
-			}
-		}
+        if (Auth::instance()->logged_in($role))
+        {
+            // return to page where login was called
+            //if ($this->session->get('requested_url')) {
+            //  url::redirect($this->session->get('requested_url'));
+            //} else {
+                url::redirect('/dash/');
+            //}
+        }
+        else
+        {
+            if (Auth::instance()->logged_in()) {
+                $this->template->title = "No Access";
+                $this->template->content = new View('/user/noaccess');
+            } else {
+                $this->template->title = "Please Login";
+                $this->template->content = new View('/user/login');
+            }
+        }
 
-	}
+    }
 
-	public function logout()
-	{
-		Auth::instance()->logout();
-		url::redirect('dash');
-	}
+    public function logout()
+    {
+        Auth::instance()->logout();
+        url::redirect('dash');
+    }
 }
